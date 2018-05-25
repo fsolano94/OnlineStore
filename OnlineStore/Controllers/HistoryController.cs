b141fd890f59962eccd52d7ed5641268fddebcc4
Franky Solano <fsolano@nevada.unr.edu>
2018-05-24 21:16:18 -0700

    Added History Controller and View

---
+++ OnlineStore/Controllers/HistoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore.Controllers
+{
+    public class HistoryController : Controller
+    {
+        [Route("/History")]
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "History";
+
+            return View() ;
+        }
+    }
+}
